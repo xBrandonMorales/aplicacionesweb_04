50f85c11dc9155faf8249bc20028aabc6a79798c
Aldo Jesus Martinez Larios <1722110193@utectulancingo.edu.mx>
2024-02-08 17:00:52 +0000

    CREATED message : error

--- aplicacion_02/mvc/controllers/index.py
+++ aplicacion_02/mvc/controllers/index.py
@@ -5,7 +5,8 @@ render = web.template.render('mvc/views/')
 class Index:
     def GET(self):
         try:
-            return render.index()
+            return render.indece()
         except Exception as e:
             print(f"Error 101 - index {e.args}")
+            return "Upsi algo salio mal"
         
